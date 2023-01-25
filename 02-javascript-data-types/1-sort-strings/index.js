241cbb9253f28f74f4e2580c639e901c2eb619d5
rabinovic1944 <rabinovic1944@yahoo.com>
2023-01-25 00:47:07 -0500

    task 21 - local tests passed

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,25 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
+
+
 export function sortStrings(arr, param = 'asc') {
 
+
+if  (param === 'asc') {
+        const arrnew = [...arr].sort((a, b) => a.localeCompare(b,['ru-Ru','en-US'],{caseFirst:"upper"}));
+        return arrnew;
+    }
+
+else if (param === 'desc') {
+        const arrnew = [...arr].sort((a, b) => a.localeCompare(b,['ru-Ru','en-US'], {caseFirst:"upper"})).reverse();
+        return arrnew;
+    } 
+
+else {
+        console.log('ERROR: wrong sorting order paramener: ', param, '/n "asc" or "desc" parameters only ');
+        return null;
+        }
+
 }
+//['ru','en'],
